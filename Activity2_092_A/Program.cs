d1e2121e061cbbb2f3306f15f62647179690364a
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-10-19 17:30:41 +0700

    kondisi while dan swap element

--- Activity2_092_A/Program.cs
+++ Activity2_092_A/Program.cs
@@ -59,6 +59,24 @@ namespace QuickSort
             j = high;
 
             pivot = arr[low];
+
+            while (i <= j)
+            {
+                //search for an element greater than pivot
+                while ((arr[i] <= pivot) && (i <= high))
+                {
+                    i++;
+                    cmp_count++;
+                }
+                cmp_count++;
+
+                if (i < j) //if the greater element is on the left of the element
+                {
+                    //swap the element at index i with the element at index j
+                    swap(i, j);
+                    mov_count++;
+                }
+            }
         }
     }
 }
