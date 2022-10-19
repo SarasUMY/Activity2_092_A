fcf38461e8504b95612a4c30f35370e3b8ab7d35
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-10-19 17:38:21 +0700

    kondisi if (low < j)

--- Activity2_092_A/Program.cs
+++ Activity2_092_A/Program.cs
@@ -77,6 +77,19 @@ namespace QuickSort
                     mov_count++;
                 }
             }
+            //j now contains the index of the last element in the sorted list
+
+            if (low < j)
+            {
+                //move the pivot to its correct position in the list
+                swap(low, j);
+                mov_count++;
+            }
+            //sort the list on the left of pivot using quick sort
+            q_sort(low, j - 1);
+
+            //sort the list on the right of pivot using quick sort
+            q_sort(j + 1, high);
         }
     }
 }
