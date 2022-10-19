25c2fa48d8875ba4151d13281a55ef1ba099cb6d
TA1092 <bagas.saras.ft21@mail.umy.ac.id>
2022-10-19 17:12:40 +0700

    membuat method swap

--- Activity2_092_A/Program.cs
+++ Activity2_092_A/Program.cs
@@ -36,5 +36,10 @@ namespace QuickSort
                 arr[i] = Int32.Parse(s1);
             }
         }
+        //swaps the element at index x with the element at index y
+        void swap(int x, int y)
+        {
+
+        }
     }
 }
